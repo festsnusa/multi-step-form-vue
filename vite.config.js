d67689719f020dcee98a972efb30137713f8e583
festsnusa <lukatimof@mail.ru>
2023-05-10 14:29:50 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/multi-step-form-vue/',
   plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // }
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
     ],
   },
 })
